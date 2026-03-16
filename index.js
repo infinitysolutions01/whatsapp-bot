539680ad59df4e2bbd992e384c89bafd8633d128
infinitysolutions01 <infinitysolutions.india01@gmail.com>
2026-03-16 23:18:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const client = new Client();
-
+const client = new Client({
+  puppeteer: {
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  }
+});
 client.on('qr', (qr) => {
     console.log('Scan this QR with WhatsApp');
     qrcode.generate(qr, {small: true});
