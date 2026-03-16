641638fdaa34bc2e73fb7bdca7ba5feadaec3d1c
infinitysolutions01 <infinitysolutions.india01@gmail.com>
2026-03-16 23:07:54 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const client = new Client();
+
+client.on('qr', (qr) => {
+    console.log('Scan this QR with WhatsApp');
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('WhatsApp Bot Ready!');
+});
+
+client.initialize();
+
+app.get('/send', async (req,res)=>{
+    const number = req.query.number;
+    const msg = req.query.msg;
+
+    const chatId = number + "@c.us";
+
+    await client.sendMessage(chatId, msg);
+
+    res.send("Message Sent");
+});
+
+app.listen(port, ()=>{
+    console.log("Server running on port " + port);
+});
